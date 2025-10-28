bd902b49a3ab1fdaac0229f67d6cdb46e892316e
Sjanrie S Hendrikz <108018913+19SJah22@users.noreply.github.com>
2025-10-28 11:59:39 +0200

    Update seaborn_example.py

--- 02_example_scripts/seaborn_example.py
+++ 02_example_scripts/seaborn_example.py
@@ -1,1 +1,15 @@
+import seaborn as sns
+import matplotlib.pyplot as plt
 
+# Load a sample dataset
+tips = sns.load_dataset("tips")
+
+# Scatter plot
+sns.scatterplot(x="total_bill", y="tip", data=tips)
+plt.title("Bill vs Tip")
+plt.show()
+
+# Box plot
+sns.boxplot(x="day", y="total_bill", data=tips)
+plt.title("Total Bill by Day")
+plt.show()
