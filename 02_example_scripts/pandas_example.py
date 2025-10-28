427e9913ce489d6e3c28774873be8eb1d29894d8
Sjanrie S Hendrikz <108018913+19SJah22@users.noreply.github.com>
2025-10-28 11:56:03 +0200

    Update pandas_example.py

--- 02_example_scripts/pandas_example.py
+++ 02_example_scripts/pandas_example.py
@@ -7,12 +7,12 @@ df = pd.read_csv("https://raw.githubusercontent.com/mwaskom/seaborn-data/master/
 print(df.head())
 
 # Select specific columns
-age_sex = df[["age", "sex"]]
-print(age_sex.head())
+age_gender = df[["age", "gender"]]
+print(age_gender.head())
 
 # Filter rows
 adults = df[df["age"] > 18]
 print(adults.head())
 
 # Group and summarize
-print(df.groupby("sex")["age"].mean())
+print(df.groupby("gender")["age"].mean())
